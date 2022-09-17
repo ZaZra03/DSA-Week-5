78a1eca89e3be347fd3ecdf1c36166c026e32720
Ezra Micah Malsi <66657798+ZaZra03@users.noreply.github.com>
2022-09-17 21:28:06 +0800

    Change file name

--- src/PrimeAndCompositeNumbers.java
+++ src/PrimeAndCompositeNumbers.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class PrimeNumbers {
+public class PrimeAndCompositeNumbers {
 	public static int[] array;
 	public static int size;
 	public static int prime;
